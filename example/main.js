f8f17e3fb2452d891bc03117c16f3658c733ad52
Bruno Luis Panuto Silva <ThePanuto@users.noreply.github.com>
2015-02-06 13:37:26 -0200

    Update main.js

--- example/main.js
+++ example/main.js
@@ -4,18 +4,24 @@ window.onload = function () {
         width: window.innerWidth - 10,
         height: window.innerHeight - 10
     });
-    drawer.register('square', ['x', 'y', ['width', 10], ['height', 10]], function (square) {
+    drawer.register('square', Drawer.square, function (square) {
         this.beginPath();
         this.rect(square.x, square.y, square.width, square.height);
         this.stroke();
         this.closePath();
     });
-    drawer.register('circle', ['x', 'y', ['radius', 20], 'startAngle', ['endAngle', Math.PI*2]], function (circle) {
+    drawer.register('circle', Drawer.circle, function (circle) {
         this.beginPath();
         this.arc(circle.x, circle.y, circle.radius, circle.startAngle, circle.endAngle);
         this.stroke();
         this.closePath();
     });
-    drawer.circle.draw({x: 200, y: 335, radius: 100});
-    drawer.square.draw({x: 300, y: 398, width: 100, height: 100});
+    drawer.register('custom-drawing', [Drawer.coordinates, 'width', 'height'], function (custom) {
+        this.beginPath();
+        this.fillRect(custom.x, custom.y, custom.width, custom.height);
+        this.closePath();
+    });
+    drawer.circle.draw({ x: 200, y: 335, radius: 100 });
+    drawer.square.draw({ x: 300, y: 398, width: 100, height: 100 });
+    drawer['custom-drawing'].draw({ x: 10, y: 45, width: 300, height: 40 });
 };
